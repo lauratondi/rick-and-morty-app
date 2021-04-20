d15a7764c5c2a03c6985aafb1b85aea24f2356a3
Lauramars <lauramars85@gmail.com>
2021-04-20 17:25:20 +0200

    Removed comments

--- src/components/Relateds.vue
+++ src/components/Relateds.vue
@@ -7,36 +7,20 @@
     aria-expanded="false"
     aria-controls="collapseExample"
   >
-    Get related
+    Suggestions
   </button>
 
   <div class="collapse" id="collapseExample">
-    <div id="related">
-      <div v-for="related in relateds" :key="related.id">
-        <!-- <div class="card text-center" :class="related.status">
-          <div class="card-header">
-            {{ related.name }}
-          </div>
-          <div class="card-body">
-            <img :src="related.image" alt=".." />
-            <p class="card-text"></p>
-            <p><b>Origin:</b> {{ related.origin.name }}</p>
-            <p><b>Location:</b> {{ related.location.name }}</p>
-          </div>
-          <div class="card-footer">
-            {{ related.status }} | {{ related.species }}
-          </div>
-        </div> -->
-        <Character
-          v-if="Object.keys(related).length !== 0"
-          :name="related.name"
-          :species="related.species"
-          :image="related.image"
-          :status="related.status"
-          :origin="related.origin"
-          :location="related.location"
-        />
-      </div>
+    <div v-for="related in relateds" :key="related.id">
+      <Character
+        v-if="Object.keys(related).length !== 0"
+        :name="related.name"
+        :species="related.species"
+        :image="related.image"
+        :status="related.status"
+        :origin="related.origin"
+        :location="related.location"
+      />
     </div>
   </div>
 </template>
